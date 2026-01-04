078045afacc1bc1cd19d897472c66a8d89e1b8bc
kian <kian.kethamreddy@gmail.com>
2026-01-04 18:37:38 +0000

    Build and Test code

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ===== BUILD STARTED =====
+echo Compiling code...
+echo ===== BUILD SUCCESS =====
+exit /b 0
---
+++ test.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ===== TEST STARTED =====
+echo Running tests...
+echo ===== TEST PASSED =====
+exit /b 0
